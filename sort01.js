8d45659e5a8b6ee9481166a4dd61e6c5d70060c4
Harsh <52203103+harshndroid@users.noreply.github.com>
2024-08-04 23:16:41 +0530

    Create sort01.js

---
+++ sort01.js
@@ -0,0 +1,12 @@
+let arr = [1, 0, 1, 0, 1, 0, 1, 0];
+let p0 = 0; // start pointer
+let p1 = arr.length-1; // end pointer
+
+while (p0 < p1) {
+  if (arr[p0] === 0) p0++;
+  else {
+    // swap
+    [arr[p0], arr[p1]] = [arr[p1], arr[p0]];
+    p1--;
+  }
+}
