1dde95c1917431380cb980d5ad0338fbbe2e5043
Harsh <52203103+harshndroid@users.noreply.github.com>
2024-08-04 22:48:37 +0530

    Update subarrayIndicesOfSumK.js

--- subarrayIndicesOfSumK.js
+++ subarrayIndicesOfSumK.js
@@ -1,7 +1,8 @@
 // Function to find a continuous sub-array which adds up to a given number.
     subarraySum(arr, n, s) {
         // your code here
-        // 2 1 => 1
+        // 1 2 3 7 6 => 12
+        // return [2, 4]
         let sum=0, i=0, j=0, found=false, map={};
         for(i=0; i<n; i++){
             sum+=arr[i];
