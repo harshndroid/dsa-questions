aff29185644b2ec7d55d856edeeda6dd23472e8c
Harsh <52203103+harshndroid@users.noreply.github.com>
2024-08-12 00:29:36 +0530

    Update sort01.js

--- sort01.js
+++ sort01.js
@@ -2,7 +2,7 @@ let arr = [1, 0, 1, 0, 1, 0, 1, 0];
 let p0 = 0; // start pointer
 let p1 = arr.length-1; // end pointer
 
-while (p0 < p1) {
+while (p0 <= p1) {
   if (arr[p0] === 0) p0++;
   else {
     // swap
