d27dff4cf4162bc3ab14129f5fe2cfba2691f3ca
Harsh <52203103+harshndroid@users.noreply.github.com>
2025-01-26 00:38:18 +0530

    Update maxSumSubArrayValue.js

--- maxSumSubArrayValue.js
+++ maxSumSubArrayValue.js
@@ -3,7 +3,7 @@ function maxSubArray(nums) {
     let maxSum = nums[0];
     for(let i = 0; i < nums.length; i++){
         currSum += nums[i];
-        if(currSum > maxSum) maxSum=currSum;
+        maxSum = Math.max(currSum, maxSum);
         if(currSum < 0) currSum=0;
     }
     return maxSum;
